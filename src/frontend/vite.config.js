1e1fcd840806e4989838a4356e7e13a05c9dd7a9
Efremov Egor <efremovegor936@gmail.com>
2024-02-23 18:46:54 +0300

    delete 1 file, update 5 files and create 2 files

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 			// '/api': 'http://api:8000'
 			'/api': {
 				target: 'http://api:8000',
-				changeOrigin: true,
+
 				secure: false,
 				rewrite: (path) => path.replace(/^\/api/, '')
 			}
